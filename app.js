0433c37fe899e590040ae0c34ae79636a8814530
Anurag Kumar <anuragsingh4512@gmail.com>
2021-08-03 11:57:32 +0530

    cash register done

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var cashInput = document.querySelector(".cash-input")
 var cashGiven = document.querySelector("#cash-given")
 var divOutput = document.querySelector("#output")
 var returnTable = document.querySelector(".return-table")
+var numberNote = document.querySelectorAll(".no-of-note")
+
+const arrayOfNotes = [2000, 500, 100, 20, 10, 5, 1]
+console.log(arrayOfNotes.length)
 
 console.log(returnTable)
 console.log(billAmt)
@@ -22,7 +26,7 @@ function clickHandlerNext(){
     divOutput.style.display = "none"
     console.log("clicked")
     console.log(billAmt.value)
-    if(billAmt.value > 0){
+    if(billAmt.value > 0 ){
         nextBtn.style.display= "none"
         cashInput.style.display = "block"
     }
@@ -43,26 +47,32 @@ function clickHandlerCheck(){
     divOutput.style.display = "none"
     returnTable.style.display = "none"
 
-    if(cashGiven.value>billAmt.value){
+    if(Number(cashGiven.value)>Number(billAmt.value) && Number(billAmt.value)>0){
         returnTable.style.display = "block"
         console.log("a " +billAmt.value)
         console.log("a " +cashGiven.value)
+        const amountToReturn = cashGiven.value - billAmt.value
+        calculateChange(amountToReturn)
     }
     else {
 
         divOutput.style.display = "block"
         returnTable.style.display = "none"
 
-        if(cashGiven.value<billAmt.value && cashGiven.value> 0){
+        if(Number(cashGiven.value)<Number(billAmt.value) && Number(cashGiven.value)> 0){
+            console.log(typeof(billAmt.value))
             console.log("b "+billAmt.value)
             console.log("b "+cashGiven.value)
             divOutput.innerText = "Cash given is less than required Bill amount."
         }
         
-        else if(cashGiven.value === billAmt.value){
+        else if(Number(cashGiven.value) === Number(billAmt.value)){
             divOutput.innerText = "No amount should be returned."
             console.log("c " +billAmt.value)
             console.log("c " +cashGiven.value)
+        }
+        else if(Number(billAmt.value)<0){
+            divOutput.innerText = "Bill amount Entered is not valid. Please enter valid amount."
         }
 
         else {
@@ -72,6 +82,17 @@ function clickHandlerCheck(){
         }
 
     }
+}
+
+function calculateChange(amountToReturn){
+    let sum = 0
+        for(let i=0; i<arrayOfNotes.length;i++){
+            const noOfNotes = Math.trunc(amountToReturn/arrayOfNotes[i])
+            numberNote[i].innerText = noOfNotes
+            amountToReturn = amountToReturn % arrayOfNotes[i]
+            sum += noOfNotes
+        }
+        numberNote[arrayOfNotes.length].innerText = sum
 }
 
 nextBtn.addEventListener("click",clickHandlerNext)
